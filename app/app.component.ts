ad392f06dabf04652369fc29829aa2667166cd76
dearzoe <2421113739@qq.com>
2016-10-11 15:05:18 +0800

    Signed-off-by: dearzoe <2421113739@qq.com>

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: '<h1>My First Angular App(u10k)</h1>'
 })
 export class AppComponent { }
